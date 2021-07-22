1f4b6c85271f95639fdba794a7f925662a82bafc
eshavkun <evgeni@leapdao.org>
2021-07-22 14:01:14 +0200

    registry and agreement deployment

--- scripts/deployPartnersRegistry.js
+++ scripts/deployPartnersRegistry.js
@@ -1,16 +1,31 @@
 /* eslint no-use-before-define: "warn" */
 const chalk = require("chalk");
 const { ethers } = require("hardhat");
-const { deploy } = require("./utils")
+const { deploy } = require("./utils");
+
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 const main = async () => {
     const deployerWallet = ethers.provider.getSigner();
     const deployerWalletAddress = await deployerWallet.getAddress();
-    const distributedTownAddress = "0xc91be9B71A1f0e79393898b9263cb77eF7682D79";
+    const distributedTownAddress = "0x559Cc95dBcA574a684FE23AcCa3a9ae49d5cF4f3";
+    let defaultSupportedTokensAddress = "0x19C1D1f6c04bEf9eE82e8a97Dd89d8d228e3472B";
 
     console.log("\n\n 📡 Deploying...\n");
 
-    const partnersRegistry = await deploy("PartnersRegistry", [distributedTownAddress]);
+    
+    if (defaultSupportedTokensAddress == "") {
+        const DefaultSupportedTokens = await ethers.getContractFactory("DefaultSupportedTokens");
+        const defaultSupportedTokens = await DefaultSupportedTokens.deploy();
+        await defaultSupportedTokens.deployed();
+        defaultSupportedTokensAddress = defaultSupportedTokens.address;
+
+        console.log("Default Supported Tokens library: ", defaultSupportedTokensAddress);
+    }
+
+    const partnersRegistry = await deploy("PartnersRegistry", [distributedTownAddress, ZERO_ADDRESS, ZERO_ADDRESS, ZERO_ADDRESS], {}, {
+        DefaultSupportedTokens: defaultSupportedTokensAddress
+    });
     await partnersRegistry.deployed();
 
     console.log(
